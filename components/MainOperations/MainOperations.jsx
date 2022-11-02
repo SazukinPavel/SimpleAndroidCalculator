0b4cce0901c4cdc9dd2d2d411c070e17d4166d6f
SazukinPavel <sazukinpavelal@gmail.com>
2022-11-02 12:51:59 +0300

    add main operations actions

---
+++ components/MainOperations/MainOperations.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MyButton from '../MyButton'
+
+export default function MainOperations() {
+    return (
+        <>
+            <MyButton text='AC' />
+            <MyButton text='()' />
+            <MyButton text='del' />
+            <MyButton text='=' />
+        </>
+    )
+}
+

b6d51b691a71c40d7042b3f61dc7f702fafffb6e
SazukinPavel <sazukinpavelal@gmail.com>
2022-11-02 11:38:16 +0300

    add app slice

---
+++ store/slices/AppSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState={}
+
+export const appSlice=createSlice({
+    name:'app-slice',
+    initialState,
+    reducers:{
+
+    }
+})
+
+export const appReducer=appSlice.reducer

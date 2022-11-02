927027c800cd687ceafd3a2cd580df413fa65735
SazukinPavel <sazukinpavelal@gmail.com>
2022-11-02 12:54:17 +0300

    change app

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
-import Numbers from './components/Numbers';
-import { View, Text } from 'react-native';
-import { useState } from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import AppView from './views/AppView';
 
 const App = () => {
-
-  const [val, setVal] = useState('Hello')
-
   return (
     <Provider store={store}>
-      <View>
-        <Text>{val}</Text>
-        <Numbers />
-      </View>
+     <AppView/>
     </Provider>
   );
 };

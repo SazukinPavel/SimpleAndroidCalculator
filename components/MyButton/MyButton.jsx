3206ad865382dc0a0bea5beaa7446168175f80a2
SazukinPavel <sazukinpavelal@gmail.com>
2022-11-02 12:53:07 +0300

    fix button align

--- components/MyButton/MyButton.jsx
+++ components/MyButton/MyButton.jsx
@@ -3,27 +3,23 @@ import { Pressable, StyleSheet, Text } from 'react-native'
 
 export default function ({ text, onPress }) {
     return (
-        <Pressable style={styles.action} onPress={onPress}><Text>{text}</Text></Pressable>
+        <Pressable style={styles.action} onPress={onPress}>
+            <Text style={styles.text}>{text}</Text>
+        </Pressable>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 30,
-        flexDirection: 'row',
-        margin: 10,
-        flexWrap: 'wrap'
-    },
     action: {
         alignItems: 'center',
         justifyContent: 'center',
-        paddingVertical: 12,
-        paddingHorizontal: 32,
         borderRadius: 4,
         elevation: 3,
         width: 80,
         height: 80,
-        margin: 5
+        margin: 3
+    },
+    text: {
+        textAlign: 'center',
     }
 })

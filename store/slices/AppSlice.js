04fdd8770b23b98a2cb0857a297e67c0e261a32e
SazukinPavel <sazukinpavelal@gmail.com>
2022-11-02 12:53:37 +0300

    add calculate string actions

--- store/slices/AppSlice.js
+++ store/slices/AppSlice.js
@@ -1,13 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState={}
+const initialState = { calculateString: '' }
 
-export const appSlice=createSlice({
-    name:'app-slice',
+export const appSlice = createSlice({
+    name: 'app-slice',
     initialState,
-    reducers:{
-
+    reducers: {
+        resetCalclulateString(state) {
+            state.calculateString = ''
+        },
+        replaceCalculateString(state, action) {
+            state.calculateString = action.payload
+        },
+        pushInCalculateString(state, action) {
+            state.calculateString += action.payload
+        }
     }
 })
 
-export const appReducer=appSlice.reducer
+export const { replaceCalculateString, resetCalclulateString, pushInCalculateString } = appSlice.actions
+
+export const appReducer = appSlice.reducer

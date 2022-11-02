7b9e3256eff787552b0e519c5970b457f94ea77a
SazukinPavel <sazukinpavelal@gmail.com>
2022-11-02 11:38:49 +0300

    add store in app

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import Numbers from './components/Numbers';
-import { View,Text } from 'react-native';
+import { View, Text } from 'react-native';
 import { useState } from 'react';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 const App = () => {
 
-  
   const [val, setVal] = useState('Hello')
 
   return (
-    <View>
-      <Text>{val}</Text>
-      <Numbers/>
-    </View>
+    <Provider store={store}>
+      <View>
+        <Text>{val}</Text>
+        <Numbers />
+      </View>
+    </Provider>
   );
 };
 
